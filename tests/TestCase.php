06723a26894a2ddf2b419e3cfe41384e6be0051a
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-06 19:43:53 +0100

    Setup the enivornment.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,10 +16,26 @@ abstract class TestCase extends Orchestra
     {
         parent::setUp();
 
-        //
+        $this->loadMigrationsFrom(__DIR__ . '/Database/migrations');
     }
 
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $config = $app->get('config');
 
+        $config->set('database.default', 'sqlite');
+        $config->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
 
     /**
      * Get package providers.
