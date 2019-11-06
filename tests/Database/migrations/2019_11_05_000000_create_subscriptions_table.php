16d51b8e973aa617a9ec7f4fcdfc610d521775d0
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-06 19:43:31 +0100

    Adds a test migration.

---
+++ tests/Database/migrations/2019_11_05_000000_create_subscriptions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->expires();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('subscriptions');
+    }
+}
