17c06716ce8666137df018f103c09ab7f0ad0642
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-07 08:03:07 +0000

    Apply fixes from StyleCI

--- src/ModelExpiresServiceProvider.php
+++ src/ModelExpiresServiceProvider.php
@@ -36,12 +36,12 @@ class ModelExpiresServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             Blueprint::macro('expires', function ($column = 'expires_at', $precision = 0) {
-                /** @var \Illuminate\Database\Schema\Blueprint $this */
+                /* @var \Illuminate\Database\Schema\Blueprint $this */
                 $this->timestamp($column, $precision)->nullable();
             });
 
             Blueprint::macro('dropExpires', function ($column = 'expires_at') {
-                /** @var \Illuminate\Database\Schema\Blueprint $this */
+                /* @var \Illuminate\Database\Schema\Blueprint $this */
                 $this->dropSoftDeletes($column);
             });
         }
