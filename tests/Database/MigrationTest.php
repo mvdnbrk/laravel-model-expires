c43e869d7d7a6c8794d7eeeb7a881bb6c469856c
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-08 13:08:06 +0100

    Refactor test.

--- tests/Database/MigrationTest.php
+++ tests/Database/MigrationTest.php
@@ -2,32 +2,19 @@
 
 namespace Mvdnbrk\ModelExpires\Tests\Database;
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Mvdnbrk\ModelExpires\Tests\TestCase;
 
 class MigrationTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->loadMigrationsFrom(__DIR__.'/migrations');
-    }
-
     /** @test */
-    public function it_runs_the_migrations()
+    public function it_can_add_the_expires_at_column()
     {
-        $this->assertEquals([
-            'id',
-            'expires_at',
-            'created_at',
-            'updated_at',
-        ], Schema::getColumnListing('subscriptions'));
-    }
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->expires();
+        });
 
-    /** @test */
-    public function it_has_the_correct_column_type()
-    {
         $this->assertEquals('datetime', Schema::getColumnType('subscriptions', 'expires_at'));
     }
 }
