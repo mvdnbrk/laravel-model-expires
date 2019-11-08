ddac9403d2c2847ad4a12e37ef2c44baf9fb228f
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-08 20:01:24 +0100

    Formatting.

--- src/Expirable.php
+++ src/Expirable.php
@@ -27,6 +27,19 @@ trait Expirable
     }
 
     /**
+     * Set the "expires at" column for an instance.
+     *
+     * @param  \DateTimeInterface|\DateInterval|int|null  $ttl
+     * @return void
+     */
+    public function setExpiresAtAttribute($ttl)
+    {
+        $seconds = $this->getSeconds($ttl);
+
+        $this->attributes[$this->getExpiresAtColumn()] = $seconds ? Carbon::now()->addSeconds($seconds) : null;
+    }
+
+    /**
      * Determine if the model instance has expired.
      *
      * @return bool
@@ -48,56 +61,6 @@ trait Expirable
         $expiresAt = $this->{$this->getExpiresAtColumn()};
 
         return $expiresAt && $expiresAt->isFuture();
-    }
-
-    /**
-     * Get the name of the "expires at" column.
-     *
-     * @return string
-     */
-    public function getExpiresAtColumn()
-    {
-        return defined('static::EXPIRES_AT') ? static::EXPIRES_AT : 'expires_at';
-    }
-
-    /**
-     * Get the fully qualified "expires at" column.
-     *
-     * @return string
-     */
-    public function getQualifiedExpiresAtColumn()
-    {
-        return $this->qualifyColumn($this->getExpiresAtColumn());
-    }
-
-    /**
-     * Set the "expires at" column for an instance.
-     *
-     * @param  \DateTimeInterface|\DateInterval|int|null  $ttl
-     * @return void
-     */
-    public function setExpiresAtAttribute($ttl)
-    {
-        $seconds = $this->getSeconds($ttl);
-
-        $this->attributes[$this->getExpiresAtColumn()] = $seconds ? Carbon::now()->addSeconds($seconds) : null;
-    }
-
-    /**
-     * Calculate the number of seconds for the given TTL.
-     *
-     * @param  \DateTimeInterface|\DateInterval|int|null  $ttl
-     * @return int
-     */
-    protected function getSeconds($ttl)
-    {
-        $duration = $ttl ? $this->parseDateInterval($ttl) : 0;
-
-        if ($duration instanceof DateTimeInterface) {
-            $duration = Carbon::now()->diffInRealSeconds($duration, false);
-        }
-
-        return (int) $duration > 0 ? $duration : 0;
     }
 
     /**
@@ -144,5 +107,42 @@ trait Expirable
     {
         return $query->where($this->getExpiresAtColumn(), '>', $this->freshTimestamp())
             ->orWhereNull($this->getExpiresAtColumn());
+    }
+
+    /**
+     * Get the name of the "expires at" column.
+     *
+     * @return string
+     */
+    public function getExpiresAtColumn()
+    {
+        return defined('static::EXPIRES_AT') ? static::EXPIRES_AT : 'expires_at';
+    }
+
+    /**
+     * Get the fully qualified "expires at" column.
+     *
+     * @return string
+     */
+    public function getQualifiedExpiresAtColumn()
+    {
+        return $this->qualifyColumn($this->getExpiresAtColumn());
+    }
+
+    /**
+     * Calculate the number of seconds for the given TTL.
+     *
+     * @param  \DateTimeInterface|\DateInterval|int|null  $ttl
+     * @return int
+     */
+    protected function getSeconds($ttl)
+    {
+        $duration = $ttl ? $this->parseDateInterval($ttl) : 0;
+
+        if ($duration instanceof DateTimeInterface) {
+            $duration = Carbon::now()->diffInRealSeconds($duration, false);
+        }
+
+        return (int) $duration > 0 ? $duration : 0;
     }
 }
